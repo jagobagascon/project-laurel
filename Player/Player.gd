02fc3d5dd430ef5d1f6871b75ec516040f571f88
Jagoba <jagoba.gascon@gmail.com>
2020-08-03 11:19:10 +0200

    modify collisiion layers and add gravity

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,37 +1,24 @@
 extends KinematicBody
 
-const SPEED = 2
+const SPEED =  2
+const G = 25
+var moving_direction = Vector3()
 
-var projection_plane = Plane.PLANE_XY
 
 onready var actionsManager = $"/root/ActionsManager"
 
 func _physics_process(delta):
 	_character_movement(delta)
 
-func _process(_delta):
-	# show / hide character actions
-	if actionsManager.action_ready():
-		var action = actionsManager.current_action()
-		_show_action(action)
-		if Input.is_action_just_pressed("player_action"):
-			print("Action: " + action.to_string())
-	else:
-		_hide_action()
-
-func _show_action(action):
-	$PlayerUI/Action.show()
-	var label_pos = $Camera.unproject_position($HintsTarget.to_global(Vector3()))
-	$PlayerUI/Action.set_position(label_pos)
-	$PlayerUI/Action/Key.text = "E"
-	$PlayerUI/Action/Text.text = action.to_string()
-	
-func _hide_action():
-	$PlayerUI/Action.hide()
-
 func _character_movement(delta):
-	var wanted_direction: Vector3 = _get_direction_by_input()
-	var _collision = move_and_collide(wanted_direction * SPEED * delta)
+	var direction = _get_direction_by_input()
+	
+	moving_direction.x = direction.x * SPEED
+	moving_direction.y += direction.y - G * delta
+	moving_direction.z = direction.z * SPEED
+	
+	moving_direction = move_and_slide(moving_direction, Vector3.UP)
+
 
 func _get_direction_by_input() -> Vector3:
 	var direction: Vector3 = Vector3()
@@ -55,7 +42,30 @@ func _get_direction_by_input() -> Vector3:
 
 	if direction.length() == 0:
 		animation = "still"
-		
+	
 	$Sprite.animation = animation
+	
 	return direction.normalized()
+
+
+func _process(_delta):
+	# show / hide character actions
+	if actionsManager.action_ready():
+		var action = actionsManager.current_action()
+		_show_action(action)
+		if Input.is_action_just_pressed("player_action"):
+			print("Action: " + action.to_string())
+	else:
+		_hide_action()
+
+func _show_action(action):
+	$PlayerUI/Action.show()
+	var label_pos = $Camera.unproject_position($HintsTarget.to_global(Vector3()))
+	$PlayerUI/Action.set_position(label_pos)
+	$PlayerUI/Action/Key.text = "E"
+	$PlayerUI/Action/Text.text = action.to_string()
+	
+func _hide_action():
+	$PlayerUI/Action.hide()
+
 
