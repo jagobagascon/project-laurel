843d5891093a3e97ff8076a4ef892cc7105e74e1
Jagoba <jagoba.gascon@gmail.com>
2020-08-06 15:59:52 +0200

    Improved player movement animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,28 +1,31 @@
 extends KinematicBody
 
-const SPEED =  2
+const MAX_SPEED =  2
+const TERMINAL_SPEED = 20
 const ACCELERATION = .25
-const G = 25
+const G = .98
 var velocity = Vector3()
 var grounded = true
 
 # for slopes
 onready var actionsManager = $"/root/ActionsManager"
+onready var animationTree = $AnimationTree
+onready var animationState = $AnimationTree.get("parameters/playback")
 
-func _physics_process(delta):
-	_character_movement(delta)
+func _physics_process(_delta):
+	_character_movement()
 
-func _character_movement(delta):
+func _character_movement():
 	var direction: Vector3 = _get_direction_by_input()
 	_update_animation(direction)
 	
 	if direction.length() == 0:
 		velocity = velocity.move_toward(Vector3.ZERO, ACCELERATION)
 	else:
-		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
 	
 	if not grounded:
-		velocity.y -= G * delta
+		velocity.y -= G
 	else:
 		# if we dont apply some negative force is_on_floor_returns false
 		# but if we apply full gravity while on floor, player movement gets
@@ -43,21 +46,15 @@ func _get_direction_by_input() -> Vector3:
 	return direction.normalized()
 
 func _update_animation(direction: Vector3):
-	var animation = "still"
-	if direction.x < 0:
-		animation = "left"
-	elif direction.x > 0:
-		animation = "right"
-	
-	if direction.z < 0:
-		animation = "back"
-	elif direction.z > 0:
-		animation = "front"
-
 	if direction.length() == 0:
-		animation = "still"
-	
-	$Sprite.animation = animation
+		animationState.travel("Still")
+	else:
+		var direction2d = Vector2(direction.x, -direction.z)
+		animationState.travel("Walk")
+		# only update blend position when we are moving.
+		# It will be remembered when stopping
+		animationTree.set("parameters/Still/blend_position", direction2d)
+		animationTree.set("parameters/Walk/blend_position", direction2d)
 
 func _process(_delta):
 	# show / hide character actions
